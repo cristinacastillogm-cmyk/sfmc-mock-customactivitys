198fbde9fd96092d0e0f83d9bbb6b900054c0e42
cristinacastillogm-cmyk <cristinacastillogm@gmail.com>
2026-04-24 13:34:29 +0200

    Update customactivity.js

--- public/customactivity.js
+++ public/customactivity.js
@@ -1,7 +1,24 @@
 var connection = new Postmonger.Session();
+var activity = {};
 
-connection.trigger('ready');
+connection.on('initActivity', function (payload) {
+  if (payload) {
+    activity = payload;
+  }
+  // Notificar a SFMC que la actividad está lista
+  connection.trigger('ready');
+});
+
+connection.on('requestedTokens', function (tokens) {
+  // Tokens JWT de SFMC disponibles aquí si los necesitas
+  console.log('Tokens recibidos:', tokens);
+});
 
 connection.on('clickedNext', function () {
-    connection.trigger('updateActivity');
+  // Al pulsar "Siguiente/Done" en el modal, guardamos la actividad
+  connection.trigger('updateActivity', activity);
+});
+
+connection.on('clickedBack', function () {
+  connection.trigger('ready');
 });
