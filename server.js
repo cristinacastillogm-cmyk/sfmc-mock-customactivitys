be8b0b9f5a2f5d52391eb16437d07e78e55b7876
cristinacastillogm-cmyk <cristinacastillogm@gmail.com>
2026-04-24 12:29:23 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
-app.use(express.json());
-
-// endpoint que usa SFMC
-app.post('/execute', (req, res) => {
-  console.log('Datos recibidos:', JSON.stringify(req.body, null, 2));
-  res.status(200).send("OK");
-});
-
-// servir archivos (config.json, html...)
+// ESTA LÍNEA ES LA CLAVE: Permite que se vea el config.json
 app.use(express.static(__dirname));
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log('Servidor funcionando'));
+// Esta permite que se vea lo que hay en la carpeta public
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.post('/execute', (req, res) => {
+    res.status(200).send('Execute');
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Servidor corriendo en puerto ${port}`);
+});
